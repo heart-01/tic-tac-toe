ae858f5fd28027d1d3143b1d9874a04b46050c78
Siwat Jermwatthana <siwat.jermwatthana@gmail.com>
2025-11-29 23:31:52 +0700

    Feat: Add MongoDB connection utility with caching mechanism

---
+++ src/config/mongodb.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+/**
+ * Cached connection for MongoDB.
+ */
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
